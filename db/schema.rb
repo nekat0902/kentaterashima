fa3846498338e88f1e403c241f39e47ae8ad9943
nekat0902 <shinehuman@gmail.com>
2019-12-14 23:52:16 +0900

    s

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_14_132521) do
-
-  create_table "inquiries", force: :cascade do |t|
-    t.string "name"
-    t.text "message"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
