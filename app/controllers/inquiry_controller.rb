72228cf8f336c748f56647b9058072edcbc5ab15
nekat0902 <shinehuman@gmail.com>
2019-12-14 23:50:40 +0900

    destory

--- app/controllers/inquiry_controller.rb
+++
@@ -1,7 +0,0 @@
-class InquiryController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-end
--- app/models/inquiry.rb
+++
@@ -1,5 +0,0 @@
-class Inquiry < ApplicationRecord
-  validates :name, presence: true, length: {maximum:255},
-                  format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
-  validates :message, presence: true
-end
