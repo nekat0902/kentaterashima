56479251a757a278dabe4bbd2749b76ceb762542
nekat0902 <shinehuman@gmail.com>
2019-12-15 23:19:19 +0900

    kkk

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'contacts/new'
-  get 'contacts/create'
-  get 'inquiry/new'
-  get 'inquiry/create'
+  get 'contacts/new'　=> 'contacts#new'
+  post 'contacts/create' => 'contacts#create'
+
   root 'home#new'
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
